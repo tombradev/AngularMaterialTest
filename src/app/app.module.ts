ac421e2c54a5e8ed760d87f1998bec3842f9bccd
tombradev <tombradev@gmail.com>
2019-08-09 11:48:35 +0700

    Adding centralized location for material modul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+/* imported from newly generated components */
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
+
+/* imported new genereated / exported modules*/
+import { MaterialModule} from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
-    EmployeeComponent
+    EmployeeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,26 @@
+/* 
+This file will contain centrelized import modules for angular material and other modules
+All imported material module will have an export 
+*/
+
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/* additional import 
+ */
+import * as Material from '@angular/material';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Material.MatToolbarModule
+  ], 
+  exports: [
+    Material.MatToolbarModule
+
+  ]
+})
+export class MaterialModule { } // the name of the link to other imports
